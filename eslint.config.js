225a01b26cb888abfbd7ec3cc1e0001f62d5fdc9
Yogihafidh <hafidhyogi@gmail.com>
2025-01-18 15:37:41 +0700

    feat: display cabin data

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
   { ignores: ["dist"] },
@@ -24,8 +24,8 @@ export default [
       "react-refresh": reactRefresh,
     },
     rules: {
-      "no-unused-vars": "off",
       "react/prop-types": "off",
+      "no-unused-vars": "off",
       "react/jsx-no-target-blank": "off",
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
