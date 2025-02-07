7ff44771ca5e7da51f8f2587c720ebec34a3e702
Yogihafidh <hafidhyogi@gmail.com>
2025-02-07 16:25:10 +0700

    feat(detail booking page): add detail booking page feature

--- src/features/bookings/useBooking.js
+++ src/features/bookings/useBooking.js
@@ -1,51 +1,19 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { getBookings } from "../../services/apiBookings";
-import { useSearchParams } from "react-router-dom";
-import { PAGE_SIZE } from "../../utils/constant";
+import { useQuery } from "@tanstack/react-query";
+import { getBooking } from "../../services/apiBookings";
+import { useParams } from "react-router-dom";
 
 export function useBooking() {
-  const queryClient = useQueryClient();
-  const [searchParams] = useSearchParams();
-
-  // Filter
-  const filterValue = searchParams.get("status");
-  const filter =
-    !filterValue || filterValue === "all"
-      ? null
-      : { field: "status", value: filterValue };
-
-  // Sorting
-  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
-  const [field, direction] = sortByRaw.split("-");
-  const sortBy = { field, direction };
-
-  // Pagenation
-  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+  const { bookingId } = useParams();
 
   const {
     isLoading,
     error,
-    data: { data: bookings, count } = {},
+    data: booking,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy, page],
-    queryFn: () => getBookings(filter, sortBy, page),
-    keepPreviousData: true,
+    queryKey: ["booking", bookingId],
+    queryFn: () => getBooking(bookingId),
+    retry: false,
   });
 
-  // Prefatching
-  const pageCount = Math.ceil(count / PAGE_SIZE);
-
-  if (page < pageCount)
-    queryClient.prefetchQuery({
-      queryKey: ["bookings", filter, sortBy, page + 1],
-      queryFn: () => getBookings(filter, sortBy, page + 1),
-    });
-
-  if (page > 1)
-    queryClient.prefetchQuery({
-      queryKey: ["bookings", filter, sortBy, page - 1],
-      queryFn: () => getBookings(filter, sortBy, page - 1),
-    });
-
-  return { isLoading, bookings, error, count };
+  return { isLoading, booking, error };
 }
