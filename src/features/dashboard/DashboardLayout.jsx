1d6dcd5cc3644c0520b5e7e27ba22ee19d573cbf
Yogihafidh <hafidhyogi@gmail.com>
2025-02-17 19:06:03 +0700

    feat(statistcs) : add display statistics in dashboard page

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import { useRecentBookings } from "../bookings/useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "../bookings/useRecentStays";
+import Stats from "./Stats";
+import { useCabin } from "../cabins/useCabin";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +11,32 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading: isLoadingBookings, bookings } = useRecentBookings();
+  const {
+    isLoading: isLoadingStays,
+    stays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoadingCabin } = useCabin();
+
+  if (isLoadingBookings || isLoadingStays || isLoadingCabin) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Todays activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart Sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
---
+++ src/features/dashboard/Stats.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/prop-types */
+import {
+  HiOutlineBanknotes,
+  HiOutlineBriefcase,
+  HiOutlineCalendarDays,
+  HiOutlineChartBar,
+} from "react-icons/hi2";
+import Stat from "./Stat";
+import { formatCurrency } from "../../utils/helpers";
+
+function Stats({ bookings, confirmedStays, numDays, cabinCount }) {
+  const numBookings = bookings.length;
+  const sales = bookings.reduce((acc, curr) => acc + curr.totalPrice, 0);
+  const checkins = confirmedStays.length;
+  const occupation =
+    confirmedStays.reduce((acc, curr) => acc + curr.numNights, 0) /
+    (numDays * cabinCount);
+
+  return (
+    <>
+      <Stat
+        icon={<HiOutlineBriefcase />}
+        title="Bookings"
+        value={numBookings}
+        color="blue"
+      />
+      <Stat
+        icon={<HiOutlineBanknotes />}
+        title="Sales"
+        value={formatCurrency(sales)}
+        color="green"
+      />
+      <Stat
+        icon={<HiOutlineCalendarDays />}
+        title="Check ins"
+        value={checkins}
+        color="indigo"
+      />
+      <Stat
+        icon={<HiOutlineChartBar />}
+        title="Occupancy rate"
+        value={Math.round(occupation * 100) + "%"}
+        color="yellow"
+      />
+    </>
+  );
+}
+
+export default Stats;
