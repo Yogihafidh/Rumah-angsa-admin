f109dc6bd391af1d30afad6116729c934d235518
Yogihafidh <hafidhyogi@gmail.com>
2025-02-18 00:08:55 +0700

    feat(Stays current day): add display stays for the current day feature

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,16 @@
+/* eslint-disable react/prop-types */
 import Button from "../../ui/Button";
+import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const { isCheckOut, checkout } = useCheckout();
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variant="primary"
+      size="small"
+      onClick={() => checkout(bookingId)}
+      disabled={isCheckOut}
+    >
       Check out
     </Button>
   );
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from "./Stats";
 import { useCabin } from "../cabins/useCabin";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -18,7 +19,6 @@ function DashboardLayout() {
   const { isLoading: isLoadingBookings, bookings } = useRecentBookings();
   const {
     isLoading: isLoadingStays,
-    stays,
     confirmedStays,
     numDays,
   } = useRecentStays();
@@ -34,8 +34,8 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Todays activity</div>
-      <DurationChart confirmedStays={confirmedStays}/>
+      <TodayActivity />
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
